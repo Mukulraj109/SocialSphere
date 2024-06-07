df1ffdf9e3d82683c0fafc4e4e794a96bf67da5c
Mukul <mukulraj756@gmail.com>
2024-06-07 19:50:34 +0530

    connect to mongoose atlas

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import ConnectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+
+ConnectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log("server is running on port", process.env.PORT);
+        })
+    })
+    .catch(error => { console.log("Mongodb connection failed !!", error) })
